2894f8ef450048dd4f6b6bffff8419c1dd55bb86
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-01 10:22:32 -0700

    pushing code

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-// grabs the data function
-async function fetchLeMansWinners() {
-    try {
-      const response = await fetch('http://localhost:3000/'); // Assuming db.json is in the same directory
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return null;
-    }
-  }
-  
-  // Example usage:
-  fetchLeMansWinners().then(data => {
-    if (data) {
-      console.log(data); // This will log the fetched data to the console
-      // You can process the data further here
-    } else {
-      console.log('Failed to fetch data.');
-    }
-  });
+function appendTimeline(car){
+  const timeLine = document.querySelector("#car-timeline")
+
+}
+
+function grab1920s() {
+  const nav = document.querySelector("the1920")
+  fetch("http://localhost:3000/1920s")
+    .then(r => r.json())
+    .then(cars => {
+      appendTimeline(cars)
+    })
+}
+grab1920s()
