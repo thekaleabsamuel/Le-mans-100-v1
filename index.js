f40b58ae20dcf2d054e9c0cf1cd44e75847b6a83
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-01 11:29:29 -0700

    reworked the database to be more usable

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-function appendTimeline(car){
+function appendTimeline(car ) {
   const timeLine = document.querySelector("#car-timeline")
-  
+  const addCar = document.createElement("img")
+  addCar.src =
+    console.log(cars1920s)
+
 }
+appendTimeline()
 
 function grab1920s() {
   const nav = document.querySelector("the1920")
   fetch("http://localhost:3000/1920s")
     .then(r => r.json())
-    .then(cars => {
-      appendTimeline(cars)
-    })
+    .then(cars => { appendTimeline(cars) })
 }
 grab1920s()
