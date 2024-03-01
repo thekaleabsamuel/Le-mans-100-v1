aaa91abb896a0262c8f223d1e99fabedfabb047d
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-01 14:58:40 -0700

    got the images properly linked

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-function appendTimeline(car ) {
+function appendTimeline(cars) {
   const timeLine = document.querySelector("#car-timeline")
-  const addCar = document.createElement("img")
-  addCar.src =
-    console.log(cars1920s)
-
+  console.log(cars)
+  cars.map(cars => {
+    const addCar = document.createElement("img")
+    addCar.src = cars.image
+    timeLine.append(addCar)
+  })
 }
-appendTimeline()
 
 function grab1920s() {
-  const nav = document.querySelector("the1920")
-  fetch("http://localhost:3000/1920s")
-    .then(r => r.json())
-    .then(cars => { appendTimeline(cars) })
+  const nav = document.querySelector("#the1920")
+  nav.addEventListener('click', () => {
+    fetch("http://localhost:3000/1920s")
+      .then(r => r.json())
+      .then(cars => { appendTimeline(cars) })
+  })
 }
 grab1920s()
